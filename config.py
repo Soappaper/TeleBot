122d66bad3bd243a69420650bb883fb6b7b10ec5
Soappaper <132600542+Soappaper@users.noreply.github.com>
2023-06-12 13:22:21 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+exchanges = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
+
+TOKEN = "6230403146:AAH97rB0IWqVqR0K3h2jUsADT679Mr2NT74"
---
+++ main.py
@@ -0,0 +1,33 @@
+import telebot
+
+from config import *
+from extensions import Convertor, APIException
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def start(message: telebot.types.Message):
+    text = "Приветствие!"
+    bot.send_message(message.chat.id, text)
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for i in exchanges.keys():
+        text = '\n'.join((text, i))
+    bot.reply_to(message, text)
+
+@bot.message_handler(content_types=['text'])
+def converter(message: telebot.types.Message):
+    try:
+        base, sym, amount = message.text.split()
+    except ValueError as e:
+        bot.reply_to(message, 'Неверное количество параметров')
+    try:
+        new_price = Convertor.get_price(base, sym, amount)
+        bot.reply_to(message, f"Цена {amount} {base} в {sym} : {new_price}")
+    except APIException as e:
+        bot.reply_to(message, f'Ошибка в команде:\n{e}')
+
+
+bot.polling()
